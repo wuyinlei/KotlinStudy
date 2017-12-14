be8c8627f7b3e20aea6c31ec636906cf653d8561
wuyinlei <power1118wu@gmail.com>
2017-12-14 17:42:33 +0800

    测试kotlin简单转换

--- app/src/main/java/ruolan/com/kotlinstudy/MainActivity.kt
+++ app/src/main/java/ruolan/com/kotlinstudy/MainActivity.kt
@@ -1,12 +1,17 @@
 package ruolan.com.kotlinstudy
 
+
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        message.text = "helloworld"
     }
 }
